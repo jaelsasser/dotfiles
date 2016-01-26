9ed33d2828f14ac50209f51ab1d0e6853ae96b89
Josh Elsasser <josh@elsasser.ca>
2016-01-25 22:06:56 -0800

    Remembered that tmux existed.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,15 @@ ln_flags=-svn
 if [ "$(uname)" = 'Darwin' ]; then
   # MacOS X
   has_osx='1'
+  ln ${ln_flags} ${src}/tmux/tmux.conf ${tgt}/.tmux.conf
 else
   # Linux
   has_linux='1'
   ln ${ln_flags} ${src}/i3 ${tgt}/.i3
 fi
+
+# make sure we have a conf dir
+mkdir -p ${tgt}/.config/
 
 # link git config files 
 ln ${ln_flags} ${src}/git/gitconfig ${tgt}/.gitconfig
@@ -20,11 +24,9 @@ ln ${ln_flags} ${src}/git/gitconfig ${tgt}/.gitconfig
 git submodule update --init --recursive
 
 # link base16-shell theme
-mkdir -p ${tgt}/.config/
 ln ${ln_flag} ${src}/theme/base16-shell/ ${tgt}/.config/base16-shell
 
 # link vim and neovim config files 
-mkdir -p ${tgt}/.config/
 ln ${ln_flags} ${src}/nvim/ ${tgt}/.config/nvim
 ln ${ln_flags} ${src}/nvim/init.vim ${tgt}/.vimrc
 ln ${ln_flags} ${src}/nvim/ ${tgt}/.vim
