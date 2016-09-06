3b69e8c21a934b04935affbd75180c2bc23f2a42
Josh Elsasser <josh@elsasser.ca>
2016-09-05 18:29:08 -0700

    vicmd/viins was a failure

--- zsh/editor.zsh
+++ zsh/editor.zsh
@@ -1,5 +1,5 @@
 # vi editing mode
-bindkey -v
+bindkey -e
 
 # 10ms for key sequences
 KEYTIMEOUT=1
@@ -7,48 +7,22 @@ KEYTIMEOUT=1
 # Treat these characters as part of a word.
 WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
 
-# allow command line editing in an external editor.
-autoload -Uz edit-command-line
-zle -N edit-command-line
-bindkey -M vicmd 'v' edit-command-line
-
-# sets some emacs-like movement commands in viins and vicmd
-for mode in viins vicmd; do
-    bindkey -M $mode "\C-l" clear-screen
-
-    bindkey -M $mode "\eb" backward-word
-    bindkey -M $mode "\ef" forward-word
-
-    bindkey -M $mode "\C-b" backward-char
-    bindkey -M $mode "\C-f" forward-char
-
-    bindkey -M $mode "\C-a" beginning-of-line
-    bindkey -M $mode "\C-e" end-of-line
-
-    # Shift-Tab to reverse complete
-    bindkey -M $mode "^[[Z" reverse-menu-complete
-
-    # I can't live without C-u everywhere, even if it's not vi-like
-    bindkey -M $mode "\C-u" kill-whole-line
-done
+# Shift-Tab to reverse complete
+bindkey -M emacs "^[[Z" reverse-menu-complete
 
 # better incremental search (shamelessly stolen from prezto)
 if (( $+widgets[history-incremental-pattern-search-backward] )); then
-    bindkey -M vicmd "?" history-incremental-pattern-search-backward
-    bindkey -M vicmd "/" history-incremental-pattern-search-forward
+    bindkey -M emacs "?" history-incremental-pattern-search-backward
+    bindkey -M emacs "/" history-incremental-pattern-search-forward
 else
-    bindkey -M vicmd "?" history-incremental-search-backward
-    bindkey -M vicmd "/" history-incremental-search-forward
+    bindkey -M emacs "?" history-incremental-search-backward
+    bindkey -M emacs "/" history-incremental-search-forward
 fi
 
-# sets some vi-like kill commands in viins
-bindkey -M viins "\C-d" delete-char
-bindkey -M viins "\e-d" kill-word
-bindkey -M viins "\C-k" kill-line
-bindkey -M viins "\C-w" backward-kill-word
-
-# expand in the middle of a word
-bindkey -M viins "\C-I" expand-or-complete
+# allow command line editing in vicmd (^X^V)
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey -M vicmd 'v' edit-command-line
 
 # expands .... to ../.. in viins
 function expand-dot-to-parent-directory-path {
