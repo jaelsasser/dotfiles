f771a9e803c383a729b2f7c6ed33e3d87cca2198
Josh Elsasser <josh@elsasser.ca>
2017-04-20 22:51:07 -0700

    Strip unused plugins from .vimrc, build on init.el

--- emacs/init.el
+++ emacs/init.el
@@ -20,7 +20,6 @@
     (require 'use-package))
 (require 'diminish)
 (require 'bind-key)
-
 (defun byte-compile-init-dir ()
   "Byte-compile all your dotfiles."
   (byte-recompile-directory user-emacs-directory 0))
@@ -38,70 +37,63 @@
 	      x-select-enable-clibpoard t ; use the system clipboard
 	      )
 
+(set-frame-font "Hack 12") ; sensible font
 (global-font-lock-mode t) ; syntax-highlighting
 
 ;; TODO: working dark theme
 ;; leuven-theme is great, but it's still a light theme
-(use-package leuven-theme
-  :ensure t
+(use-package leuven-theme :ensure t
   :init (load-theme 'leuven t))
 
 ;; Set the path variable to match the system PATH
-(use-package exec-path-from-shell
-  :ensure t
+(use-package exec-path-from-shell :ensure t
   :config (exec-path-from-shell-initialize))
 
+;; Better command discoverability
+(use-package which-key :ensure t :defer t
+  :init (which-key-mode t)
+  :config
+  (setq which-key-popup-type 'minibuffer))
+
 ;; Pull in pdf-tools
-(use-package pdf-tools
-  :ensure t
+(use-package pdf-tools :ensure t :defer t
   :config (pdf-tools-install)
           (setq-default pdf-view-display-size 'fit-page))
 
-(use-package magit
-  :ensure t
-  :bind ("C-x g" . magit-status))
+(use-package magit :ensure t :defer t
+  :bind ("C-x g" . magit-status)) 
 
-(use-package evil
-  :ensure t
-  :init (evil-mode t)
+;; TODO: configure (http://ebzzry.io/en/emacs-pairs/)
+(use-package smartparens :ensure t :defer t)
+
+(use-package evil :ensure t
+  :init (evil-mode 1)
   :config
-  (setcdr evil-insert-state-map nil)
-  (define-key evil-insert-state-map [escape] 'evil-normal-state)
-  (setq evil-default-state 'evil
-        evil-search-module 'evil-search
+  (defalias 'evil-insert-state 'evil-emacs-state) ;mask insert-state
+  (setq evil-default-state 'emacs
+        evil-search-module 'isearch
 	evil-magic 'very-magic
 	evil-want-fine-undo-want-fine-undo t
-	evil-want-change-word-to-end t))
-
-(use-package evil-snipe
-  :ensure t
+	evil-want-change-word-to-end t)
+  :bind (:map evil-emacs-state-map (([escape] . evil-normal-state)
+				    ("C-[" . evil-normal-state))))
+  
+(use-package evil-snipe :ensure t
   :diminish evil-snipe-local-mode
   :init (evil-snipe-mode 1))
 
-(use-package undo-tree
-  :ensure t
+(use-package undo-tree :ensure t
   :diminish undo-tree-mode)
 
 ;; better completion with ivy, counsel, and friends
-(use-package ivy
-  :ensure t
+(use-package ivy :ensure t
   :diminish ivy-mode
   :init (ivy-mode 1))
 
-(use-package swiper
-  :ensure t
-  :bind
-  (([remap isearch-forward]  . swiper)
-   ([remap isearch-backward] . swiper))
-  :config
-  (setq swiper-action-recenter t))
+(use-package counsel :ensure t :defer t)
 
-(use-package counsel
-  :defer t)
-
-(use-package company
-  :ensure t
-  :defer t
+(use-package company :ensure t :defer t
+  :diminish company-mode
   :init (add-hook 'after-init-hook 'global-company-mode)
   :bind ("C-;" . company-complete-common))
 
