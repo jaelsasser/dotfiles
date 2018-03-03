565b8dd49c1940fa4b05e26b3087f0757f182d14
Josh Elsasser <josh@elsasser.ca>
2018-03-03 15:02:30 -0800

    bootstrap.el: include MELPA archives in bootstrap

--- emacs/conf/bootstrap.el
+++ emacs/conf/bootstrap.el
@@ -1,7 +1,7 @@
 ;; bootstrap.el --- compatibility shims and core functions
 
 ;;;
-;;; Shim for xdg-data-home on Emacs < 25.1
+;;; Shim for xdg-data-home on Emacs < 25.0.50
 ;;;
 (defvar user-emacs-data "~/.local/share/emacs"
   "${XDG_CONFIG_HOME:-~/.local/share}/emacs")
@@ -11,7 +11,6 @@
 (defun user-emacs-file (file)
   (concat user-emacs-data "/" file))
 
-
 (setq custom-file (concat user-emacs-data "/custom.el")
       package-user-dir (format (concat user-emacs-data "/elpa-%s/")
                                emacs-major-version)
@@ -20,12 +19,18 @@
       package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("melpa" . "https://melpa.org/packages/"))
-      package-archive-priorities '(("melpa-stable" . 1)
-                                   ("elpa" . 2)
+      package-archive-priorities '(("elpa" . 2)
+				                   ("melpa-stable" . 1)
                                    ("melpa" . 0))
-      package-enable-at-startup nil
       use-package-enable-imenu-support t
       use-package-always-ensure t)
+
+;; Spacemacs is kind enough to provide a nice MELPA / ELPA mirror for when those go down
+;; (setq
+;;  package-archives
+;; '(("melpa-stable" . "https://raw.githubusercontent.com/syl20bnr/spacemacs-elpa-mirror/master/stable-melpa/")
+;;   ("melpa" . "https://raw.githubusercontent.com/syl20bnr/spacemacs-elpa-mirror/master/melpa/")
+;;   ("elpa" . "https://raw.githubusercontent.com/syl20bnr/spacemacs-elpa-mirror/master/gnu/")))
 
 (require 'package)
 (package-initialize)
