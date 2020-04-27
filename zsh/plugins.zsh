29437c414a49f8e9ea64adf6210373d597ad1d4e
Josh Elsasser <jelsasser@appneta.com>
2020-04-27 11:12:01 -0700

    zsh: simplify prompt configuration
    Drops the substring-matching history (in favor of the bash-style C-r) and
    gets rid of the __prompt_pure_dynamic_symbol() hook that I never used.

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,15 +1,3 @@
-zplug "rupa/z", use:z.sh
 zplug "zsh-users/zsh-completions"
-
 # load syntax highlighting second-last
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
-
-# load substring search last
-function __zsh_history_substring_search_hook_load {
-    for mode in emacs viins vicmd; do \
-        bindkey -M $mode "^P" history-substring-search-up; \
-        bindkey -M $mode "^N" history-substring-search-down; \
-    done
-}
-zplug "zsh-users/zsh-history-substring-search", defer:3, \
-    hook-load:'__zsh_history_substring_search_hook_load'
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,33 +3,7 @@
 #   Run before zplug load
 #
 
-# store the prompt pure symbol in psvar
-function __prompt_pure_set_symbol {
-    case ${KEYMAP} in
-      (vicmd)      psvar[1]='>'   ;;
-      (*)          psvar[1]='>'   ;;
-    esac
-}
-
-function __prompt_pure_dynamic_symbol {
-    # register callbacks on exit/enter vicmd, redraw prompt
-    function zle-keymap-select {
-        local __old=${psvar[1]:-'no'}
-        __prompt_pure_set_symbol
-        if [[ "$__old" != "$psvar[1]" ]]; then
-            zle reset-prompt
-        fi
-    }
-    zle -N zle-keymap-select
-
-    # register callback on prompt precmd to redraw symbol
-    autoload -Uz add-zsh-hook
-    add-zsh-hook precmd __prompt_pure_set_symbol
-
-    psvar[1]=❯
-    export PROMPT=${PROMPT/❯/%v}
-}
-
 zplug "mafredri/zsh-async", defer:0
-zplug "sindresorhus/pure", defer:1, at:v1.7.0, \
-    hook-load:'__prompt_pure_dynamic_symbol'
+zplug "sindresorhus/pure", defer:1
+
+PROMPT='%(?.%F{magenta}$.%F{red}$)%f '
