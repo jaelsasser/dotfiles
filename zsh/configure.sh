7016eee64759fa0a6a26224d2a6e8a4154b2c390
Josh Elsasser <josh@elsasser.ca>
2022-08-06 15:59:46 -0700

    zsh: create /etc/zshenv if it's not present

--- zsh/configure.sh
+++ zsh/configure.sh
@@ -1,22 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-ENVFILE="$HOME"/.zshenv
-for FILE in /etc/zshenv /etc/zsh/zshenv; do
-    if [[ -f "$FILE" ]]; then
-        ENVFILE="$FILE"
-        break
-    fi
-done
+if [ -f /etc/zsh/zshenv ]; then
+    readonly zshenv_file="/etc/zsh/zshenv"
+else
+    readonly zshenv_file="/etc/zshenv"
+fi
 
-# make sure zsh looks for its zshrc in the right spot on launch
-ZDOTINIT='export ZDOTDIR=${XDG_CONFIG_BASE:-$HOME/.config/}zsh'
-if ! grep -q "$ZDOTINIT" "$ENVFILE" >/dev/null 2>&1; then
-    if ! echo $ZDOTINIT | sudo tee -a "$ENVFILE"; then
-        echo "Failed to set custom ZDOTDIR"
+# make sure zsh looks for .zshrc in the right spot on launch
+snippet='test -d "${XDG_CONFIG_BASE:-$HOME/.config/}zsh" && export ZDOTDIR=${XDG_CONFIG_BASE:-$HOME/.config/}zsh'
+if ! grep -q "$snippet" "$zshenv_file" >/dev/null 2>&1; then
+    if ! echo "$snippet" | sudo tee -a "$zshenv_file"; then
+        printf 'Failed to set ZDOTDIR vis %s, symlinking into $HOME\n' "$zshenv_file" >&2
+        ln -sf "$(cwd)/zsh/.zshenv" "$HOME/.zshenv"
     fi
 fi
 
 ZPLUG_HOME=${XDG_DATA_BASE:-"$HOME/.local/share"}/zsh/zplug
-if ! [[ -d $ZPLUG_HOME ]]; then
+if [ ! -d $ZPLUG_HOME ]; then
     git clone https://github.com/zplug/zplug --depth 1 "$ZPLUG_HOME"
 fi
