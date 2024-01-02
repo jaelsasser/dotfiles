d3dcd4d2bfb92a7f6c27292b996779408d3e5fbf
Josh Elsasser <josh@elsasser.ca>
2024-01-02 12:13:15 -0800

    zsh: ever-so-slightly modernize prompt.zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,8 +2,12 @@
 # SET ZSH PROMPT
 #   Run before zplug load
 #
+zplug "mafredri/zsh-async", from:github, defer:0
+zplug "sindresorhus/pure", from:github, use:pure.zsh as:theme, defer:1
 
-zplug "mafredri/zsh-async", defer:0
-zplug "sindresorhus/pure", defer:1
+zstyle ':prompt:pure:git:stash' show no
+zstyle ':prompt:pure:git:fetch' only_upstream yes
+PURE_GIT_PULL=0
 
-PROMPT='%(?.%F{magenta}$.%F{red}$)%f '
+zstyle ':prompt:pure:prompt:success' color magenta
+zstyle ':prompt:pure:prompt:error' color red
