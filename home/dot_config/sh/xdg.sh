4c7c314fc72b8bec9abe3e1e12843ab13513dd46
Josh Elsasser <josh@elsasser.ca>
2026-05-31 22:55:57 -0700

    sh: state of mind
    Vibed.

--- home/dot_config/sh/xdg.sh
+++ home/dot_config/sh/xdg.sh
@@ -4,6 +4,7 @@
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 
 if ! [ -n "$XDG_RUNTIME_DIR" ]; then
     export XDG_RUNTIME_DIR="${TMPDIR:-/tmp/$USER}"
